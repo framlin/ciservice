f3c5cb9b8e4072bd2849d7db397d03320fe995d5
Wolfgang Egger <w.egger@framlin.com>
2018-04-14 12:18:20 +0200

    remove sass take ci-stiles from framlin

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 WDIR=$1
-forever start -w --watchDirectory $WDIR -l ciservice.log -a $WDIR/server/run.js
+forever start -w --watchDirectory $WDIR -l ciservice.log -a $WDIR/node_modules/http-server/bin/http-server --cors -p 8088
