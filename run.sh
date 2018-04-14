738371ed156ebed998e4109fe176e5f5d9e2cd0f
Wolfgang Egger <w.egger@framlin.com>
2018-04-14 23:08:27 +0200

    compute host/ip use service-type for urls

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 WDIR=$1
-forever start -w --watchDirectory $WDIR -l ciservice.log -a $WDIR/node_modules/http-server/bin/http-server --cors -p 8088
+forever start -w --watchDirectory $WDIR -l ciservice.log -a $WDIR/node_modules/http-server/bin/http-server --cors -c-1 -p 8088
